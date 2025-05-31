b2c86d34488a0d644d54684b49508c0d6be6cf32
TrueValPoperty <williamtylerstreet@gmail.com>
2025-05-31 11:01:54 +0100

    Update app.py

--- app.py
+++ app.py
@@ -10,3 +10,15 @@ def home():
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 10000))  # Render sets PORT as an environment variable
     app.run(host='0.0.0.0', port=port)
+from flask import request, jsonify
+
+@app.route('/valuation', methods=['POST'])
+def valuation():
+    data = request.get_json()
+    postcode = data.get('postcode')
+
+    # Example dummy response
+    return jsonify({
+        "postcode": postcode,
+        "valuation": "£295,000"
+    })
