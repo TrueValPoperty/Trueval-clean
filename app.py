741595cac325f36c1bb327fc6a8661a8236eb02a
TrueValPoperty <williamtylerstreet@gmail.com>
2025-06-01 15:09:20 +0100

    Update app.py

--- app.py
+++ app.py
@@ -79,3 +79,12 @@ def valuation():
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
+    from train_route import retrain_model
+
+@app.route('/train', methods=['POST'])
+def train():
+    try:
+        message = retrain_model()
+        return jsonify({"status": "success", "message": message}), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
