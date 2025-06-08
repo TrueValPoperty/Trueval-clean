952b8f067ae159182513f40ec5e60efbf1b856b0
TrueValPoperty <williamtylerstreet@gmail.com>
2025-06-08 16:50:02 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import pandas as pd
 from geopy.distance import geodesic
 
 app = Flask(__name__)
+CORS(app)
 
 # Load university data once
 universities_df = pd.read_csv('universities.csv')
